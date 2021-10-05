633bab360533c61c968de713b0e684805b6dc286
ab62390 <83035212+ab62390@users.noreply.github.com>
2021-10-05 09:10:48 +0200

    Add files via upload

---
+++ precache-manifest.1b29794789f92fa027382f3d90ac6cd8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e19d659a46128401830",
+    "url": "/css/app.a386e483.css"
+  },
+  {
+    "revision": "9dee847619de8875d90a",
+    "url": "/css/chunk-vendors.e8b7fbe5.css"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "bcc9ade4dc0a9242fb13345bc293889d",
+    "url": "/img/bg-01.bcc9ade4.jpg"
+  },
+  {
+    "revision": "271b07be078109dcf6163ea20f0b15c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e19d659a46128401830",
+    "url": "/js/app.225d4410.js"
+  },
+  {
+    "revision": "9dee847619de8875d90a",
+    "url": "/js/chunk-vendors.38fe36d8.js"
+  },
+  {
+    "revision": "f04d3772c4764ec13b594e724d85b4eb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
